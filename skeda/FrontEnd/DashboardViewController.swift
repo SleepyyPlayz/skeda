c68fb880dfaa4932dcd42b1a850ab6f25269136e
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2020-07-19 00:37:11 +0800

    added sort list options menu

--- skeda/FrontEnd/DashboardViewController.swift
+++ skeda/FrontEnd/DashboardViewController.swift
@@ -2,25 +2,74 @@
 //  FirstViewController.swift
 //  skeda
 //
-//  Created by 蒋汪正 on 7/13/20.
+//  Created by Sleepyy on 7/13/20.
 //  Copyright © 2020 Icosa Studios. All rights reserved.
 //
 
 import UIKit
+import PopMenu
 
 class DashboardViewController: UIViewController {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
     
     @IBAction func sortButtonPressed(_ sender: UIButton) {
+        
+        //Create Actions for the Menu
+        var sortMethodViewActions : [PopMenuDefaultAction] = []
+        
+        //PopMenuDefaultAction(title: String?, image: UIImage?, color: Color?, didSelect: PopMenuActionHandler func (can also be closure) )
+        sortMethodViewActions.append(PopMenuDefaultAction(title: "Smart Sort", color: UIColor(named: CONSTS.Colors.PseudoWhite), didSelect: { (action) in
+            
+            //PLACEHOLDER
+            print("\(action.title ?? "error") was selected")
+            
+        }))
+        sortMethodViewActions.append(PopMenuDefaultAction(title: "Sort by Due Date", color: UIColor(named: CONSTS.Colors.PseudoWhite), didSelect: { (action) in
+            
+            //PLACERHOLDER
+            print("\(action.title ?? "error") was selected")
+            
+        }))
+        sortMethodViewActions.append(PopMenuDefaultAction(title: "Sort by Tag", color: UIColor(named: CONSTS.Colors.PseudoWhite), didSelect: { (action) in
+            
+            //PLACEHOLDER
+            print("\(action.title ?? "error") was selected")
+            
+        }))
+        sortMethodViewActions.append(PopMenuDefaultAction(title: "Sort by Date Created", color: UIColor(named: CONSTS.Colors.PseudoWhite), didSelect: { (action) in
+            
+            //PLACEHOLDER
+            print("\(action.title ?? "error") was selected")
+            
+        }))
+        
+        
+        //Create the Menu itself
+        let sortMethodViewController = PopMenuViewController(sourceView: sender, actions: sortMethodViewActions)
+        
+        sortMethodViewController.appearance.popMenuBackgroundStyle = .blurred(.light)
+        sortMethodViewController.appearance.popMenuColor.backgroundColor = .solid(fill: (UIColor(named: CONSTS.Colors.BrandAzure)!))
+        sortMethodViewController.appearance.popMenuFont = .systemFont(ofSize: 20, weight: .semibold)
+        sortMethodViewController.appearance.popMenuItemSeparator = .fill(UIColor(named: CONSTS.Colors.PseudoWhite)!, height: 1)
+        sortMethodViewController.appearance.popMenuCornerRadius = 20
+        
+        
+        
+        
+        
+        
+        present(sortMethodViewController, animated: true, completion: nil)
     }
     
     
-
-    //let alert = UIAlertController(title: <#T##String?#>, message: <#T##String?#>, preferredStyle: .actionSheet)
+    
+    
 }
 
