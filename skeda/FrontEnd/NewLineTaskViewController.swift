3fd0f4b840c4a6bb4ddfad1d9f96c1ee2b928fa5
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2020-10-11 00:49:56 +0800

    added new class

---
+++ skeda/FrontEnd/NewLineTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewLineTaskViewController.swift
+//  skeda
+//
+//  Created by 蒋汪正 on 10/10/20.
+//  Copyright © 2020 Icosa Studios. All rights reserved.
+//
+
+import UIKit
+
+class NewLineTaskViewController: UIViewController {
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.isModalInPresentation = true
+    }
+}
